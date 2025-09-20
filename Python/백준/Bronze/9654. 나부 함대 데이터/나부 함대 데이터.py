a37aa7eeeef82ed0832a616eafbaac41695b5f31
leesojunghub <158808681+leesojunghub@users.noreply.github.com>
2025-09-20 23:15:00 +0900

    [Bronze V] Title: 나부 함대 데이터, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/9654. 나부 함대 데이터/나부 함대 데이터.py
@@ -0,0 +1,11 @@
+data = [
+    ("SHIP NAME", "CLASS", "DEPLOYMENT", "IN SERVICE"),
+    ("N2 Bomber", "Heavy Fighter", "Limited", 21),
+    ("J-Type 327", "Light Combat", "Unlimited", 1),
+    ("NX Cruiser", "Medium Fighter", "Limited", 18),
+    ("N1 Starfighter", "Medium Fighter", "Unlimited", 25),
+    ("Royal Cruiser", "Light Combat", "Limited", 4),
+]
+
+for row in data:
+    print("%-15s%-15s%-11s%-10s" % row)
