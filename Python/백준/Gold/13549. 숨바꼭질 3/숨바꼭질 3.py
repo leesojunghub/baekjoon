fbd38edd67f7c7812239a25c64658f59b17e49e2
leesojunghub <158808681+leesojunghub@users.noreply.github.com>
2025-12-11 23:59:37 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 900 ms, Memory: 59360 KB -BaekjoonHub

---
+++ Python/백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -0,0 +1,23 @@
+import sys
+from collections import deque
+n, k = map(int, sys.stdin.readline().split())
+MAX = 1000000
+graph = [float('INF')] * (MAX+1)
+graph[n] = 0
+queue = deque()
+queue.append(n)
+while queue:
+    node = queue.popleft()
+    nx = node * 2
+    if nx <= MAX and graph[nx] > graph[node]:
+        graph[nx] = graph[node]
+        queue.appendleft(nx)
+    nx = node - 1
+    if nx >= 0 and graph[nx] > graph[node] + 1:
+        graph[nx] = graph[node] + 1
+        queue.append(nx)
+    nx = node + 1
+    if nx <= MAX and graph[nx] > graph[node] + 1:
+        graph[nx] = graph[node] + 1
+        queue.append(nx)
+print(graph[k])
