799485da43794f83dd73653c4d4f2f23f8f4a2a2
leesojunghub <158808681+leesojunghub@users.noreply.github.com>
2026-02-06 22:51:00 +0900

    [Silver III] Title: 킹, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Silver/1063. 킹/킹.py
@@ -0,0 +1,42 @@
+import sys
+king, stone, n = sys.stdin.readline().split()
+n = int(n)
+
+move_map = {
+    "R":  (1, 0),
+    "L":  (-1, 0),
+    "B":  (0, -1),
+    "T":  (0, 1),
+    "RT": (1, 1),
+    "LT": (-1, 1),
+    "RB": (1, -1),
+    "LB": (-1, -1),
+}
+
+kx = ord(king[0]) - ord('A')
+ky = int(king[1]) - 1
+sx = ord(stone[0]) - ord('A')
+sy = int(stone[1]) - 1
+
+def in_board(x, y):
+    return 0 <= x < 8 and 0 <= y < 8
+
+for _ in range(n):
+    move = input().strip()
+    dx, dy = move_map[move]
+
+    nkx, nky = kx + dx, ky + dy
+
+    if not in_board(nkx, nky):
+        continue
+    if (nkx, nky) == (sx, sy):
+        nsx, nsy = sx + dx, sy + dy
+
+        if not in_board(nsx, nsy):
+            continue
+        sx, sy = nsx, nsy
+
+    kx, ky = nkx, nky
+
+print(chr(kx + ord('A')) + str(ky + 1))
+print(chr(sx + ord('A')) + str(sy + 1))
