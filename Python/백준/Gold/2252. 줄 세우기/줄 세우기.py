03b755cb48cea76980bde5e60c9165eda6dfc501
leesojunghub <158808681+leesojunghub@users.noreply.github.com>
2026-04-18 19:05:02 +0900

    [Gold III] Title: 줄 세우기, Time: 192 ms, Memory: 40896 KB -BaekjoonHub

--- Python/백준/Gold/2252. 줄 세우기/줄 세우기.py
+++ Python/백준/Gold/2252. 줄 세우기/줄 세우기.py
@@ -1,12 +1,27 @@
-<html>
-<head><title>504 Gateway Time-out</title></head>
-<body>
-<center><h1>504 Gateway Time-out</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+graph = [[] for _ in range(n + 1)]
+indegree = [0] * (n + 1) 
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    indegree[b] += 1
+
+queue = deque()
+for i in range(1, n+1):
+    if indegree[i] == 0:
+        queue.append(i)
+
+result = []
+while queue:
+    now = queue.popleft()
+    result.append(now)
+    for i in graph[now]:
+        indegree[i] -= 1
+        if indegree[i] == 0:
+            queue.append(i)
+print(*result)
